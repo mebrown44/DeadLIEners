be823703fe9d74174283c434b10e87258d24c044
Abigail Koss <abigailkoss@Abigails-MacBook-Air.local>
2025-02-01 18:11:10 -0500

    email changes

--- Email.java
+++ Email.java
@@ -4,18 +4,17 @@ public class Email {
     private String user;
     private String domain;
 
+    /*
     public static void main(String args[])
     {
-        
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Please enter your email!");
         Email object = new Email(myScanner.nextLine());
 
         System.out.println(object.user);
         System.out.println(object.domain);
-        
-        
     }
+    */
     
     //Takes in full email as a string, split it into user and domain (domain starts at @)
     public Email(String s){
@@ -24,6 +23,7 @@ public class Email {
         {
             user = s.substring(0, s.indexOf("@"));
             domain = s.substring(s.indexOf("@"));
+            //System.out.println("true");
         }
 
     }
@@ -31,15 +31,20 @@ public class Email {
     //Make sure email is valid, check if has valid domain
     public boolean validEmail(String s){
         
+        String specialChars = "!#$%&'()*+,-/:;<=>?[]^_`{|}~";
 
-        if(s.contains("@") && s.contains("."))
+        if(!(s.contains("@") && s.contains(".")))
         {
-            return true;
-        }
-        else{
             return false;
         }
-        
+        for(int i =0; i < s.length(); i++)
+        {
+            if(specialChars.contains(String.valueOf(s.charAt(i))))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     public String getUserName()
@@ -47,11 +52,9 @@ public class Email {
         return user;
     }
     
-
     /*
      * public void verifyEmail(){
      * //Optionally at the end, would send a quick message to email to verify it
      * }
      */
 }
-
