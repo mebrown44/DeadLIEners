7b580cc259050f5d6182d0b327f3dfdafe535907
mebrown44 <meb471@pitt.edu>
2025-02-01 12:52:34 -0500

    Im a psychopath prolly

---
+++ Email.java
@@ -0,0 +1,20 @@
+public class Email {
+    private String user;
+    private String domain;
+
+    //Takes in full email as a string, split it into user and domain (domain starts at @)
+    public Email(String s){
+
+    }
+
+    //Make sure email is valid, check if has valid domain
+    public boolean validEmail(){
+        return false;
+    }
+
+    /*
+     * public void verifyEmail(){
+     * //Optionally at the end, would send a quick message to email to verify it
+     * }
+     */
+}
---
+++ User.java
@@ -0,0 +1,14 @@
+public class User{
+    private Email username;
+    private int passkey;
+
+    //Take in the sign in information and write it into a file
+    public User(Email u, int p){
+
+    }
+
+    //Default constructor, just so it doesn't error out, doesn't need much code
+    public User(){
+
+    }
+}
